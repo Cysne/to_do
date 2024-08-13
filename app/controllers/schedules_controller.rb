ecdc6d9b856b9f8ff0be58acbbc414d354c008dd
liziane cysne <liziane.medeiros@sefin.fortaleza.ce.gov.br>
2024-08-13 12:01:05 -0300

    wp: adiciona a logica do new e create ao controller

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -12,9 +12,17 @@ class SchedulesController < ApplicationController
   end
 
   def new
+    @schedule = Schedule.new
   end
 
   def create
+    @schedule = Schedule.new(schedule_params)
+
+    if @schedule.save
+      redirect_to schedules_path, notice: 'Cronograma criado com sucesso.'
+    else
+      render :new
+    end
   end
 
   def edit
