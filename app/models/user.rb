00a207a5d079457d5d77f42df7d7cd962b7d4742
liziane cysne <liziane.medeiros@sefin.fortaleza.ce.gov.br>
2024-08-14 11:54:52 -0300

    feat: adiciona model de user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
+# classe de usuário
 class User < ApplicationRecord
+  self.primary_key = 'id'
+  has_many :schedules
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
